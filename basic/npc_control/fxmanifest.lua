e40628a28fea1cfdb05815e79364aac2c4f8eb54
Fezz <43589668+FBFezz@users.noreply.github.com>
2020-06-04 19:18:21 +0100

    Update and rename __resource.lua to fxmanifest.lua

--- basic/npc_control/fxmanifest.lua
+++ basic/npc_control/fxmanifest.lua
@@ -16,8 +16,13 @@
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 ]]
 
+-- Resource Metadata
+fx_version 'bodacious'
+games { 'gta5' }
 
-description "npc_control"
+author 'Sighmir'
+description 'npc_control'
+version '1.0.0'
 
 client_scripts{ 
   "lib/enum.lua",
